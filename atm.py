1c259793eaaf0f441591675251f4ef15eb346059
PeacefulDev19 <285201762+PeacefulDev19@users.noreply.github.com>
2026-06-08 16:45:07 +0530

    Implement basic ATM terminal interface

---
+++ atm.py
@@ -0,0 +1,19 @@
+balance = 100
+print('Terminal ATM Interface')
+print('======================')
+print('Use appropriate keys for the required operation.')
+print('\n')
+print('\n')
+while True:
+  print('Terminal ATM Interface')
+  print('======================')
+  print('Use appropriate keys for the required operation.')
+  print('\n')
+  print('\n')
+  print('1. Check your Balance')
+  print('2. WIthdraw Amount')
+  print('3. Deposit Amount')
+  print('4. Quit;)
+  try:
+    response = int(input('Enter the key')
+  
