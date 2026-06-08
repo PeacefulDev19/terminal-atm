2533a9bf9f1b4dc019307d9fede4d0dc5b01f3b8
PeacefulDev19 <285201762+PeacefulDev19@users.noreply.github.com>
2026-06-08 17:16:10 +0530

    Update atm.py

--- atm.py
+++ atm.py
@@ -9,7 +9,7 @@ while True:
     print('3. Deposit Amount')
     print('4. Quit')
     try:
-        response = int(input('Enter the key  \n'))
+        response = float(input('Enter the key  \n'))
     except ValueError:
         print('Invalid Key! Try Again')
         continue
@@ -18,23 +18,31 @@ while True:
         continue
     elif response == 2:
         try:
-            withdraw = int(input('Enter the amount you would like to withdraw in numbers\n'))
+            withdraw = float(input('Enter the amount you would like to withdraw in numbers\n'))
         except ValueError:
             print('Invalid format! Try Again.\n')
-        balance -= withdraw
-        print('Amount was successfully withdrawn')
-        print(f'Your New Balance is ${balance}.\n')
+        if withdraw > balance:
+            print("Withdrawl amount cannot be greater than Balance")
+            continue
+        else:             
+            balance -= withdraw
+            print('Amount was successfully withdrawn')
+            print(f'Your New Balance is ${balance}.\n')
     elif response == 3:
         try:
-            deposit = int(input('Enter the amount you would like to deposit in numbers\n'))
+            deposit = float(input('Enter the amount you would like to deposit in numbers\n'))
         except ValueError:
             print('Invalid format! Try Again.')
-        balance += deposit
-        print('Amount was successfully deposited')
-        print(f'Your New Balance is ${balance}.')
+            continue
+        if deposit <= 0:
+            print("You cannot deposit negative amount or Nothing at all")
+            continue
+        else:
+            balance += deposit
+            print('Amount was successfully deposited')
+            print(f'Your New Balance is ${balance}.')
     elif response == 4:
         break
     else:
         print('The value of key should be between among 1 , 2 , 3, 4 only! Try Again.')
         continue
-
